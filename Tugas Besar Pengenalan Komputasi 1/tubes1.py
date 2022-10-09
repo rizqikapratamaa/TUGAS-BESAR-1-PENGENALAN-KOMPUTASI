6c93bc5ba5df06e741c1f63732b2260c0711c4b9
rizqikapratamaa <78368327+rizqikapratamaa@users.noreply.github.com>
2022-10-09 12:57:54 +0700

    atm.py

--- Tugas Besar Pengenalan Komputasi 1/tubes1.py
+++ Tugas Besar Pengenalan Komputasi 1/tubes1.py
@@ -1,2 +1,12 @@
-def tubes():
-  print("Tubes")
+# TUGAS BESAR 1 PENGENALAN KOMPUTASI
+# KELOMPOK 4
+# Jason Fernando (19622254)
+# Rizqika Mulia Pratama (19622258)
+# Alfaza Naufal Zakiy (19622264)
+# Chelvadinda (19622276)
+
+# Deskripsi: Program yang menjalankan fungsi ATM, mulai dari penarikan, penyetoran, dan pengecekan
+
+# KAMUS
+
+# ALGORITMA
